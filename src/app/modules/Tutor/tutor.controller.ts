6602f2d278a9ae4866a1ee1f3c34fa91dd308cd6
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-06-24 02:36:03 +0600

    Implement TutorController with CRUD operations for managing tutors

---
+++ src/app/modules/Tutor/tutor.controller.ts
@@ -0,0 +1,63 @@
+import status from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { TutorService } from "./tutor.service";
+
+const createTutor = catchAsync(async (req, res) => {
+    const tutor = await TutorService.createTutor(req.body);
+    sendResponse(res, {
+        statusCode: status.CREATED,
+        success: true,
+        message: "Tutor created successfully",
+        data: tutor,
+    });
+});
+
+const getAllTutors = catchAsync(async (req, res) => {
+    const tutors = await TutorService.getAllTutors();
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        message: "Tutors retrieved successfully",
+        data: tutors,
+    });
+});
+
+const getTutorById = catchAsync(async (req, res) => {
+    const tutor = await TutorService.getTutorById(req.params.id);
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        message: "Tutor retrieved successfully",
+        data: tutor,
+    });
+});
+
+const updateTutor = catchAsync(async (req, res) => {
+    const tutors = await TutorService.updateTutor(req.params.id, req.body);
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        message: "Tutors retrieved successfully",
+        data: tutors,
+    });
+});
+
+const deleteTutorById = catchAsync(async (req, res) => {
+    await TutorService.deleteTutorById(req.params.id);
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        message: "Tutor deleted successfully",
+        data: null
+    });
+});
+
+
+export const TutorController = {
+    createTutor,
+    getAllTutors,
+    getTutorById,
+    updateTutor,
+    deleteTutorById
+};
