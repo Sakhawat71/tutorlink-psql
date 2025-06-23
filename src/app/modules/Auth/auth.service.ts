63679a7d174e1f115b4bc9e30c6c5989e6b81da3
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-06-23 16:21:31 +0600

    Refactor authentication service to integrate Prisma for user data retrieval and enhance error handling in registration and login processes

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken';
 import { AppError } from "../../errors/AppError";
 import status from "http-status";
 import configs from "../../configs";
+import prisma from "../../utils/primsa";
+import generateToken from "../../utils/generateToken";
 
 
 // register
@@ -14,7 +16,7 @@ const registerUser = async (payLoad: IUser) => {
     if (existingUser) {
         throw new AppError(
             "User with this email already exists",
-            status.CONFLICT, 
+            status.CONFLICT,
         );
     };
     return await UserModel.create(payLoad);
@@ -24,6 +26,19 @@ const registerUser = async (payLoad: IUser) => {
 const loginUser = async (payLoad: ILoginUser) => {
 
     const { email, password } = payLoad;
+
+    const userData = await prisma.user.findUnique({
+        where: {
+            email: payload.email,
+        }
+    });
+    if (!userData) {
+        throw new AppError(
+            "user not found",
+            status.NOT_FOUND,
+        );
+    };
+
 
     const user = await UserModel.findOne({ email });
     if (!user) {
@@ -52,7 +67,7 @@ const loginUser = async (payLoad: ILoginUser) => {
 
     const JwtPayload = {
         email: user.email,
-        name : user.name,
+        name: user.name,
         role: user.role,
         id: user._id,
     };
@@ -70,6 +85,37 @@ const loginUser = async (payLoad: ILoginUser) => {
         token: accessToken
     }
 };
+
+
+const loginInToDB = async (payload: ILogin) => {
+    const userData = await prisma.user.findUnique({
+        where: {
+            email: payload.email,
+        }
+    });
+    if (!userData) {
+        throw new Error("user not found")
+    };
+
+    const isCorrectPassword: boolean = await bcrypt.compare(payload.password, userData.password);
+    if (!isCorrectPassword) {
+        throw new Error("password incurrect")
+    };
+
+    const accessToken = generateToken({
+        email: userData.email,
+        role: userData.role,
+    },
+        configs.jwt.jwt_secret as string,
+        configs.jwt.expiresin as string
+    );
+
+    return {
+        token : accessToken,
+    };
+};
+
+
 
 export const authService = {
     registerUser,
