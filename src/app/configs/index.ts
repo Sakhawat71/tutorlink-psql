0a31e13b4023b50641cf6ab32fad599914c9fb96
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-06-23 12:57:01 +0600

    Initialize project structure with essential configuration files and dependencies

---
+++ src/app/configs/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+    env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    // jwt: {
+    //     jwt_secret: process.env.JWT_SECRET,
+    //     expiresin: process.env.JWT_EXPIRES_IN,
+    // },
+    // cloudinary: {
+    //     cloud_name: process.env.CLOUDINARY_NAME,
+    //     api_key: process.env.CLOUDINARY_API_KEY,
+    //     api_secret: process.env.CLOUDINARY_API_SECRET
+    // }
+};
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { Server } from 'http';
+import app from './app';
+import configs from './app/configs';
+
+async function main() {
+    try {
+        const server: Server = app.listen(configs.port, () => {
+            console.log(`Server is running on port ${configs.port}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+    }
+}
+
+main().catch(error => {
+    console.error('Error in main function:', error);
+});
