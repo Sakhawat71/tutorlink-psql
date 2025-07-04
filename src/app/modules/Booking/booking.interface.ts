adee8d3c9a5a810f301a69c4a5cb1f4530f184aa
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-07-05 02:51:46 +0600

    Add Booking interface with BookingStatus type and properties for bookings

---
+++ src/app/modules/Booking/booking.interface.ts
@@ -0,0 +1,19 @@
+export type BookingStatus = 'PENDING' | 'CONFIRMED' | 'CANCELLED' | 'COMPLETED';
+
+export interface IBooking {
+    id?: string;
+    studentId: string;
+    tutorId: string;
+
+    date: Date;
+    duration: number;
+    price: number;
+
+    status: BookingStatus;
+
+    payment?: any;
+    review?: any;
+
+    createdAt?: Date;
+    updatedAt?: Date;
+}
