e9f671684d3d4b255394de42415f4bf66746be0a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-08-07 19:52:15 -0100

    fix user service

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,16 +1,16 @@
 import status from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { UserServices } from "./user.service";
 
 const getUser = catchAsync(async (req, res) => {
     const { id } = req.params;
-
-    
+    const result = await UserServices.getUser(id);
     sendResponse(res, {
         statusCode: status.OK,
         success: true,
         message: "Bookings retrieved successfully",
-        // data: result
+        data: result
     });
 });
 
